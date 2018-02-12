354c736ac4bb50937bc68696e8cad0253c6a54fa
Raveesh Agarwal <raveeshr503@gmail.com>
2018-02-12 08:32:15 +0530

    [SPRINT 5] Implementing the PresentClass widget, handover to design.
    A stateless widget that takes a Subject, a DateTime and builds itself.
    Implementing the state of Home and making it refresh every second.
    
    * pubspec.yaml
    * additional libraries
    * DateTime
    * NumberFormat
    * DateFormat
    * intl
    * Card
    * Center
    * Container
    * Column
    * Row
    * Expanded
    * flex
    * Text
    * Timer
    * Timer.periodic
    * initState(), dispose(), _onTimer(Timer) logic

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:present_sir/model/subject.dart';
+import 'package:present_sir/model/timetable.dart' as table;
 import 'counter.dart';
+import 'thisperiod.dart';
+import 'dart:async';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,14 +13,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreen extends State<HomeScreen> {
+  DateTime _time;
+  Subject _subject;
+  Timer _timer;
+
   @override
   void initState() {
     super.initState();
+    _time = new DateTime.now();
+    _subject = table.getSubject(_time);
+    _timer = new Timer.periodic(new Duration(seconds: 1), _onTimer);
   }
 
   @override
   void dispose() {
+    _timer.cancel();
     super.dispose();
+  }
+
+  _onTimer(Timer _timer) {
+    setState(() {
+      _time = new DateTime.now();
+      _subject = table.getSubject(_time);
+    });
   }
 
   List<Widget> generateCardList() => new List.generate(
@@ -28,19 +46,10 @@ class _HomeScreen extends State<HomeScreen> {
     return new Center(
       child: new Column(
         children: <Widget>[
-          new Expanded(
-            flex: 3,
-            child: new Card(
-              child: new Center(
-                child: new Text('This Period'),
-              ),
-            ),
-          ),
+          new Expanded(flex: 3, child: new ThisPeriod(_time, _subject)),
           new Expanded(
             flex: 8,
-            child: new ListView(
-              children: generateCardList()
-            ),
+            child: new ListView(children: generateCardList()),
           ),
         ],
       ),
---
+++ lib/widgets/thisperiod.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:present_sir/model/subject.dart';
+import 'package:intl/intl.dart';
+
+class ThisPeriod extends StatelessWidget {
+  final DateTime _time;
+  final Subject _subject;
+  final NumberFormat _percentFormatter = new NumberFormat.percentPattern();
+  final DateFormat _timeFormatter = new DateFormat.jm();
+
+  ThisPeriod(this._time, this._subject);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: new Center(
+        child: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  new Text(_subject.teacher),
+                  new Text(_timeFormatter.format(_time)),
+                  new Text(_subject.room)
+                ],
+              ),
+              new Expanded(
+                flex: 2,
+                child: new Text(_subject.name),
+              ),
+              new Expanded(
+                flex: 1,
+                child: new Row(
+                  children: <Widget>[
+                    new Text(
+                        '${_subject.noOfClassesPresent}/${_subject.noOfClasses}'),
+                    new Text(_percentFormatter.format(
+                        _subject.noOfClassesPresent / _subject.noOfClasses))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
